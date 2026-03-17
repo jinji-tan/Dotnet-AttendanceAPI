d2260e8575d90f49c9d760fe02cef42f15b1c80d
Louis Tan <louistan600@gmail.com>
2026-03-17 23:34:57 +0800

    setup

--- obj/Debug/net10.0/AttendanceAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/AttendanceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendanceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176647bcc638e8a46c54a0f8321ef2ae89fe5a8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendanceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendanceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
